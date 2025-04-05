5144add44761c2b8350dd6a0d447f190507fe6f5
thaobn20 <thaobn20@gmail.com>
2025-04-05 10:04:54 +0700

    add more function

--- zalo_zns/wizard/__init__.py
+++ zalo_zns/wizard/__init__.py
@@ -1,2 +1,3 @@
-from . import zns_test_wizard
-from . import zns_debug_wizards  # New debug wizards
+#from . import zns_test_wizard
+#from . import zns_debug_wizards  # New debug wizards
+from . import zns_template_wizard
